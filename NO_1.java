61f4fe9a44369d0bd77b488eea2e4478345f7315
Raditya Prasetyo <35413691+radityaprasetyo44@users.noreply.github.com>
2022-05-29 12:58:17 +0700

    Add files via upload

---
+++ NO_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FinalExam;
+
+/**
+ *
+ * @author User
+ */
+public class NO_1 {
+    public static void main(String[] args) {
+        String huruf = "selamat pagi, raditya!";
+        System.out.println(huruf.toUpperCase());
+        
+        huruf = "SELAMAT PAGI, RADITYA!";
+        System.out.println(huruf.toLowerCase());
+        
+        huruf = "selamat pagi, raditya!";
+        System.out.println(huruf.substring(14, 21));
+    }
+}
